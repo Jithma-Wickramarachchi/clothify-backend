a02cdd88458b613e42136d4876aaf9d9631fd4cb
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-04-15 18:45:44 +0530

    initial commit

---
+++ src/main/java/edu/icet/clothifybackend/ClothifyBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.icet.clothifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/icet/clothifybackend/ClothifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.icet.clothifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
