816a5d41eb0ba3085531ca0ed311147754ad44b3
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-04-17 15:08:39 +0530

    Add native queries to StockRepository

--- src/main/java/edu/icet/clothifybackend/repository/StockRepository.java
+++ src/main/java/edu/icet/clothifybackend/repository/StockRepository.java
@@ -1,7 +1,17 @@
 package edu.icet.clothifybackend.repository;
 
+import edu.icet.clothifybackend.dto.StockDto;
 import edu.icet.clothifybackend.entity.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface StockRepository extends JpaRepository< StockEntity, Long> {
+    @Query(value = "SELECT * FROM stock WHERE stock_id = :id ", nativeQuery = true)
+    Optional<StockEntity> getStockByStockId(@Param("id") Long stockId);
+    @Query(value = "SELECT * FROM stock", nativeQuery = true)
+    List<StockDto> getAllStocks();
 }
