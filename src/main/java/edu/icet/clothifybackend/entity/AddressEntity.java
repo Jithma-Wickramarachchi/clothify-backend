c85cb7d1d2b232ed02bfec584ba07a8c2fd2db3c
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-05-07 00:49:43 +0530

    Create AddressEntity

---
+++ src/main/java/edu/icet/clothifybackend/entity/AddressEntity.java
@@ -0,0 +1,27 @@
+package edu.icet.clothifybackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressEntity {
+    @Id
+    private Long id;
+    private String getHouseNumber;
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+}
