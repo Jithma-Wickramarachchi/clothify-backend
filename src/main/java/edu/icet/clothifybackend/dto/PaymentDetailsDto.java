2f5035072ad212523f6a415adf6be8260b68cb78
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-05-09 16:51:28 +0530

    Create PaymentDetailsDto

---
+++ src/main/java/edu/icet/clothifybackend/dto/PaymentDetailsDto.java
@@ -0,0 +1,17 @@
+package edu.icet.clothifybackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentDetailsDto {
+    private Long id;
+    private String cardHolderName;
+    private String cardNumber;
+    private String expireDate;
+    private String username;
+}
