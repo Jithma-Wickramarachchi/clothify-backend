8a256e05824f1e36f95a6569b467d7e578d45b71
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-05-18 22:45:31 +0530

    Create OrderedItemDto

---
+++ src/main/java/edu/icet/clothifybackend/dto/user/OrderedItemDto.java
@@ -0,0 +1,46 @@
+package edu.icet.clothifybackend.dto.user;
+
+import edu.icet.clothifybackend.util.Category;
+import edu.icet.clothifybackend.util.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderedItemDto {
+    private Long id;
+
+    @NotBlank(message = "name cannot be blank or null")
+    private String name;
+
+    @NotBlank(message = "description cannot be blank or null")
+    private String description;
+
+    @NotNull(message = "price cannot be null")
+    @Positive(message = "price should be a positive number")
+    private Double price;
+
+    @NotBlank(message = "category cannot be blank or null")
+    private Category category;
+
+    @NotNull(message = "count cannot be null")
+    @Positive(message = "count should be a positive number")
+    private Integer count;
+
+    @NotBlank(message = "size cannot be blank or null")
+    private Size size;
+
+    @NotNull(message = "orderId cannot be null")
+    @Positive(message = "orderId should be a positive number")
+    private Long orderId;
+
+    @NotBlank(message = "username cannot be null or blank")
+    private Long username;
+}
